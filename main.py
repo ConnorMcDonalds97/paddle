b086a43857f2135c3e705ce41791aefcb2de972e
kevin <kcao36@outlook.com>
2025-09-07 18:29:51 -0600

    add name comaprison

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from urllib.request import urlopen
+from difflib import SequenceMatcher
 import json
 import pprint
 import random
+
+
+
 response = urlopen('https://api.openf1.org/v1/drivers')
 data = json.loads(response.read().decode('utf-8'))
 
 randomInt = random.randint(0,len(data))
 
-pprint.pprint(data[randomInt])
+selectedDriver = data[randomInt]
 
+pprint.pprint(selectedDriver)
+
+print(selectedDriver['full_name'])
+
+userGuess = input('guess the driver: ')
+
+def similar(a,b):
+    return SequenceMatcher(None, a, b).ratio()
+
+if (similar(userGuess.lower() ,selectedDriver['full_name'].lower())) > 0.7:
+    print('yay!')
